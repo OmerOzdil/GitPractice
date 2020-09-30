3542a97bdd1941e9c36ee0427355acc280889ad0
Omer Ozdil <omerozdil54@gmail.com>
2020-09-30 20:30:35 +0100

    AddedInte

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -4,5 +4,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("This is coming Hello Test class");
         System.out.println("I added another line");
+        System.out.println("This is the last statement");
     }
 }
